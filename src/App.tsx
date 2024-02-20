fb42bc3fd8dc38276bf143babfc5cb09b5a5164f
NiceBoyClick <glass.ocean10@gmail.com>
2024-02-21 01:54:56 +0300

    доработан макет сайта

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
                     Здесь мы делимся интересными кейсами из наших проектов,
                     пишем про IT, а также переводим зарубежные статьи
                 </p>
-                <div className='input-container'>
+                <div className='input-container space-between'>
                     <img
                         src={loupe}
                         alt="web-app"
@@ -28,15 +28,15 @@ function App() {
                     <input type="text" placeholder="поиск по названию статьи" className='input-field'/>
                 </div>
             </div>
-            <div className='container-1'>
+            <div className='container-1 shadow'>
                 <img
                     src={main}
                     alt=""
                     className='image-main'
                 />
-                <div className='container-2'>
+                <div className='container-2 space-between'>
                     <h2 className='title-little'>Что нужно знать об эффективной интернет-рекламе?</h2>
-                    <div className='like-dislike-container'>
+                    <div className='like-dislike-container space-between'>
                         <button
                             className="like-button"
                             onClick={() => setLikeCount(likeCount + 1)}
@@ -60,6 +60,139 @@ function App() {
                     Ежедневно в сеть выходит более 5 миллиардов людей -
                     каждый из них может увидеть вашу рекламу и стать вашим потенциальным клиентом.
                 </p>
+                <div className='container-button'>
+                    <button className='button'>Читать далее</button>
+                </div>
+            </div>
+            <div className='container-compact space-between'>
+                <div className='container-3 shadow'>
+                    <img
+                        src={main}
+                        alt=""
+                        className='image-second'
+                    />
+                    <div className='container-2'>
+                        <h2 className='title-little'>Что нужно знать об эффективной интернет-рекламе?</h2>
+
+                    </div>
+                    <div className='container-button space-between'>
+                        <div className='like-dislike-container'>
+                            <button
+                                className="like-button"
+                                onClick={() => setLikeCount(likeCount + 1)}
+                                aria-label="Лайк"
+                            >
+                                <img src={like} alt="" className='like-icon'/>
+                            </button>
+                            <span>{likeCount}</span>
+                            <button
+                                className="dislike-button"
+                                onClick={() => setDislikeCount(dislikeCount + 1)}
+                                aria-label="Дизлайк"
+                            >
+                                <img src={dislike} alt="" className='dislike-icon'/>
+                            </button>
+                            <span>{dislikeCount}</span>
+                        </div>
+                        <button className='button'>Читать далее</button>
+                    </div>
+                </div>
+                <div className='container-3 shadow'>
+                    <img
+                        src={main}
+                        alt=""
+                        className='image-second'
+                    />
+                    <div className='container-2'>
+                        <h2 className='title-little'>Что нужно знать об эффективной интернет-рекламе?</h2>
+                    </div>
+                    <div className='container-button space-between'>
+                        <div className='like-dislike-container'>
+                            <button
+                                className="like-button"
+                                onClick={() => setLikeCount(likeCount + 1)}
+                                aria-label="Лайк"
+                            >
+                                <img src={like} alt="" className='like-icon'/>
+                            </button>
+                            <span>{likeCount}</span>
+                            <button
+                                className="dislike-button"
+                                onClick={() => setDislikeCount(dislikeCount + 1)}
+                                aria-label="Дизлайк"
+                            >
+                                <img src={dislike} alt="" className='dislike-icon'/>
+                            </button>
+                            <span>{dislikeCount}</span>
+                        </div>
+                        <button className='button'>Читать далее</button>
+                    </div>
+                </div>
+            </div>
+            <div className='container-compact space-between'>
+                <div className='container-3 shadow'>
+                    <img
+                        src={main}
+                        alt=""
+                        className='image-second'
+                    />
+                    <div className='container-2'>
+                        <h2 className='title-little'>Что нужно знать об эффективной интернет-рекламе?</h2>
+
+                    </div>
+                    <div className='container-button space-between'>
+                        <div className='like-dislike-container'>
+                            <button
+                                className="like-button"
+                                onClick={() => setLikeCount(likeCount + 1)}
+                                aria-label="Лайк"
+                            >
+                                <img src={like} alt="" className='like-icon'/>
+                            </button>
+                            <span>{likeCount}</span>
+                            <button
+                                className="dislike-button"
+                                onClick={() => setDislikeCount(dislikeCount + 1)}
+                                aria-label="Дизлайк"
+                            >
+                                <img src={dislike} alt="" className='dislike-icon'/>
+                            </button>
+                            <span>{dislikeCount}</span>
+                        </div>
+                        <button className='button'>Читать далее</button>
+                    </div>
+                </div>
+                <div className='container-3 shadow'>
+                    <img
+                        src={main}
+                        alt=""
+                        className='image-second'
+                    />
+                    <div className='container-2'>
+                        <h2 className='title-little'>Что нужно знать об эффективной интернет-рекламе?</h2>
+                    </div>
+                    <div className='container-button space-between'>
+                        <div className='like-dislike-container'>
+                            <button
+                                className="like-button"
+                                onClick={() => setLikeCount(likeCount + 1)}
+                                aria-label="Лайк"
+                            >
+                                <img src={like} alt="" className='like-icon'/>
+                            </button>
+                            <span>{likeCount}</span>
+                            <button
+                                className="dislike-button"
+                                onClick={() => setDislikeCount(dislikeCount + 1)}
+                                aria-label="Дизлайк"
+                            >
+                                <img src={dislike} alt="" className='dislike-icon'/>
+                            </button>
+                            <span>{dislikeCount}</span>
+                        </div>
+                        <button className='button'>Читать далее</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
